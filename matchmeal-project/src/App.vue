23a07661a234229bfc9982f0f96f0c777593684b
seho1278 <seho1278@naver.com>
2025-11-28 02:52:43 +0900

    feat: frontend server init

---
+++ matchmeal-project/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <main>
+    <CiTest title="안녕! 나는 테스트 컴포넌트야" />
+  </main>
+</template>
+
+<script setup lang="ts">
+  import CiTest from './components/CiTest.vue'
+</script>
+
+<style scoped>
+
+</style>
